96158124e7206196311fedf890e36fa0c7dd65be
Amy Barba <Amybarba70@gmail.com>
2019-11-10 19:29:29 -0800

    added server folder and router folder and files

--- server.js
+++ server.js
@@ -1,3 +1,32 @@
 // get express 
 const express = require('express');
 
+// get actionModel router 
+const actmodRouter = require('./routers/actmod-router.js');
+
+// get projectModel router 
+const projmodRouter = require('./routers/projmod-router.js');
+
+// create server object 
+const server = express();
+
+// attach server to json via express 
+server.use(express.json());
+
+// attach server to actions 
+server.use('/api/actions', actmodRouter);
+
+// attach server to projects 
+server.use('/api/projects', projmodRouter);
+
+// get from the root 
+server.get('/', (request, response) => {
+    response.status(420)
+    response.send("Hello from Amys Node and Express Challenge Project")
+});
+
+// send the server out 
+module.exports = server;
+
+
+
